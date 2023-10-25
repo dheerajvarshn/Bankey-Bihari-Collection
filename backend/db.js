806dc0621d6af6b6fe06f82340434ac9c1ccb8e9
Dheeraj varshney <110107731+dheerajvarshn@users.noreply.github.com>
2023-10-25 18:01:31 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
-const { MONGO_URL, database } = require("./config/key");
+const { database } = require("./config/key");
 
 mongoose.set({
   strictQuery: false,
 });
 
-const connection = mongoose.connect(database).catch((err) => console.log(err));
+const connection = mongoose.connect(database,{
+  useNewUrlParser:true,
+  useUnifiedTopology:true
+}).then(()=>console.log('connection successfull')).catch((err) => console.log(err))
 
 module.exports = { connection };
